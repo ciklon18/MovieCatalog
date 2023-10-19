b8aac98ba4eca81117703548d361c2f212b03c81
pupov <pupov435@gmail.com>
2023-10-19 15:43:49 +0700

    1) Completed LaunchScreen

---
+++ app/src/main/java/com/example/moviecatalog/presentation/LaunchScreen.kt
@@ -0,0 +1,54 @@
+package com.example.moviecatalog.presentation
+
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.moviecatalog.R
+import kotlinx.coroutines.delay
+
+@Composable
+fun LaunchScreen() {
+    val alpha = remember {
+        Animatable(0f)
+    }
+    LaunchedEffect(key1 = true) {
+        alpha.animateTo(
+            1f,
+            animationSpec = tween(
+                durationMillis = 1500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(2000)
+    }
+    Image(
+        painter = painterResource(R.drawable.launch_screen_background),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.fillMaxSize()
+    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .alpha(alpha.value),
+        contentAlignment = Alignment.Center,
+    ) {
+        Image(
+            painter = painterResource(R.drawable.dark_logo),
+            contentDescription = null
+        )
+    }
+}
