498bcf55743a9e656368ba0ef8decf10ffa41457
pupov <pupov435@gmail.com>
2023-10-28 17:42:07 +0700

    merge commit

--- app/src/main/java/com/example/moviecatalog/registration/di/RegistrationModule.kt
+++ app/src/main/java/com/example/moviecatalog/registration/di/RegistrationModule.kt
@@ -1,14 +1,8 @@
 package com.example.moviecatalog.registration.di
 
 import com.example.moviecatalog.common.auth.domain.repository.AuthRepository
-import com.example.moviecatalog.common.validation.domain.usecase.RegistrationValidationUseCase
 import com.example.moviecatalog.common.token.domain.usecase.SetTokenToLocalStorageUseCase
-import com.example.moviecatalog.common.validation.domain.usecase.ValidateDateUseCase
-import com.example.moviecatalog.common.validation.domain.usecase.ValidateEmailUseCase
-import com.example.moviecatalog.common.validation.domain.usecase.ValidateLoginUseCase
-import com.example.moviecatalog.common.validation.domain.usecase.ValidateNameUseCase
-import com.example.moviecatalog.common.validation.domain.usecase.ValidatePasswordUseCase
-import com.example.moviecatalog.common.validation.domain.usecase.ValidateRepeatedPasswordsUseCase
+import com.example.moviecatalog.common.validation.domain.usecase.RegistrationValidationUseCase
 import com.example.moviecatalog.registration.presentation.RegistrationViewModel
 import dagger.Module
 import dagger.Provides
